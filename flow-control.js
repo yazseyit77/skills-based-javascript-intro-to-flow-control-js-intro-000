aa313b2dcd64b5243053145cffc4c8bc7f1e5cfe
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-08 04:14:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,9 +6,9 @@ function basicTeenager(age) {
 function teenager(age) {
   if(13 <= age && age <= 19 )
   return "You are a teenager!";
-}, else{
-  return "You are not a teenager";
 }
+return "You are not a teenager";
+
 
 function ageChecker(age) {
 
