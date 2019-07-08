4e27ddde800242711d0f80f2b7a3c6f5b18b94a8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-08 04:32:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,11 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch(age){
+    case 13, 14, 15, 16, 17, 18, 19:
+    console.log("You are a teenager");
+    break;
+    default:
+     console.log("You have an age");
+  }
 }
